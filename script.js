abb4d8b75d71a54fcc611e3ce8ed9a09c8b8bb7c
surely07 <sweetpt88@gmail.com>
2023-10-23 21:22:45 +0900

    추가파일 최종

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 const options = {
-  method: 'GET',
+  method: "GET",
   headers: {
-    accept: 'application/json',
-    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MzUzMjBkNzY3YzgwZGEwZDU0ZDlkMjQxMjdlOWVkOSIsInN1YiI6IjY1MmZjOGM3MzU4ZGE3NWI2MWZhMDRlNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uj9aWONZob_7Jpf-N2mmPcPhP4I0g9jVTWaqGFETuaE'
+    accept: "application/json",
+    Authorization:
+      "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4MzUzMjBkNzY3YzgwZGEwZDU0ZDlkMjQxMjdlOWVkOSIsInN1YiI6IjY1MmZjOGM3MzU4ZGE3NWI2MWZhMDRlNCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uj9aWONZob_7Jpf-N2mmPcPhP4I0g9jVTWaqGFETuaE"
   }
 };
 
-
-fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', options)
+fetch(
+  "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1",
+  options
+)
   .then(response => response.json())
   .then(data => {
     let moviesData = [];
@@ -18,57 +21,55 @@ fetch('https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=1', opti
 
 function displayMovies(data) {
   data.forEach(movie => {
-    const moviesContainer = document.getElementById('movies-container');
+    const moviesContainer = document.getElementById("movies-container");
 
-    const card = document.createElement('div');
-    card.classList.add('movie-card');
+    const card = document.createElement("div");
+    card.classList.add("movie-card");
 
-    const img = document.createElement('img');
-    img.src = 'https://image.tmdb.org/t/p/w300' + movie['poster_path'];
+    const img = document.createElement("img");
+    img.src = "https://image.tmdb.org/t/p/w300" + movie["poster_path"];
 
-    const title = document.createElement('h2');
-    title.textContent = movie['title'];
+    const title = document.createElement("h2");
+    title.textContent = movie["title"];
 
-    const rating = document.createElement('h5');
-    rating.textContent = '⭐ Rating: ' + movie['vote_average'];
+    const rating = document.createElement("h5");
+    rating.textContent = "⭐ Rating: " + movie["vote_average"];
 
-    const overview = document.createElement('p');
-    overview.textContent = movie['overview'];
+    const overview = document.createElement("p");
+    overview.textContent = movie["overview"];
 
     card.append(img, rating, title, overview);
     moviesContainer.appendChild(card);
 
-    card.addEventListener('click', () => {
-      alert("Movie's ID : " + movie['id']);
+    card.addEventListener("click", () => {
+      alert("Movie's ID : " + movie["id"]);
     });
-  })
+  });
 
+  const searchInput = document.getElementById("search-input");
+  const searchButton = document.getElementById("search-btn");
+  const movieCards = document.querySelectorAll(".movie-card");
 
-const searchInput = document.getElementById('search-input');
-const searchButton = document.getElementById('search-btn');
-const movieCards = document.querySelectorAll('.movie-card');
+  function performSearch() {
+    const searchText = searchInput.value.toLowerCase();
 
-function performSearch() {
-  const searchText = searchInput.value.toLowerCase();
-
-  movieCards.forEach(card => {
-    const title = card.querySelector('h2').textContent.toLowerCase();
-    if (title.includes(searchText)) {
-      card.style.display = 'block';
-    } else {
-      card.style.display = 'none';
-    }
-  })
-}
-
-searchButton.addEventListener('click', function () {
-  performSearch()
-});
-
-searchInput.addEventListener('keydown', function (event) {
-  if (event.key === 'Enter') {
-    performSearch();
+    movieCards.forEach(card => {
+      const title = card.querySelector("h2").textContent.toLowerCase();
+      if (title.includes(searchText)) {
+        card.style.display = "block";
+      } else {
+        card.style.display = "none";
+      }
+    });
   }
-});
 
+  searchButton.addEventListener("click", function () {
+    performSearch();
+  });
+
+  searchInput.addEventListener("keydown", function (event) {
+    if (event.key === "Enter") {
+      performSearch();
+    }
+  });
 }
